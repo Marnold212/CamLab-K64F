9882731bb5a535dbdf6cc8cb8224ce0618e71c1c
mpa31 <marnold212.ma212@gmail.com>
2021-11-04 01:51:51 +0000

    Added toleracne for EOL char within uint data

--- K64F Python Interfacing Testing/Read_Serial_Data.py
+++ K64F Python Interfacing Testing/Read_Serial_Data.py
@@ -1,5 +1,6 @@
 import serial.tools.list_ports as port_list
 import serial
+import time
 
 ports = list(port_list.comports())
 for p in ports:
@@ -21,33 +22,45 @@ else:
             # result = device.readline()
             # print(result)
             
-            serialPort = serial.Serial( port=COM_Port, baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+            serialPort = serial.Serial( port=COM_Port, baudrate=9600, bytesize=8, timeout=1, stopbits=serial.STOPBITS_ONE)
             serialString = ""  # Used to hold data coming over UART
-    
+            Readings = 1    # Just to avoid the statement (%100 == 0)being true initially 
+            current_time = time.time()
             while 1:
+                if Readings % 101 == 0:
+                    new_time = time.time()
+                    time_interval = new_time - current_time
+                    print("Last 100 readings took %f seconds giving a rate of %f readings/second" % (time_interval, (100.0/time_interval)))
+                    current_time = new_time
+                    Readings = 1
                 # Wait until there is data waiting in the serial buffer
                 if serialPort.in_waiting > 0:
 
                     # Read data out of the buffer until a carriage return / new line is found
-                    
-                    # serialString = serialPort.read_until()    # Read until expected sequence found - by default '\n'
-                    serialString = serialPort.readline().hex()
-                    # serialString = serialPort.read_until('\n').hex()
 
-                    # Print the contents of the serial data
+                    # Note there is an inherant issue with this where, if a transmitted value has the equivalent hex value of 0a, it triggers the end of line
+                    # If we are sending unsigned bytes across channel, inevitably some will have the value of '\n' character 
+                    
+                    Bytes_Transferred = 8*2 + 1
+                    serialString = serialPort.readline()
+                    if(len(serialString) > Bytes_Transferred):
+                        print("Input too long - error")
+                    else:
+                        while(len(serialString) < Bytes_Transferred):
+                            # print(serialString)
+                            serialString += serialPort.readline()
+                    serialString = serialString.hex()  # Decode bytes into raw hex values
                     try:
-                        # print(serialString.decode("Ascii"))
-                        # print(serialString)
-                        if(len(serialString) == 6):   # 6 = (No. Bytes * 2 since each byte is 2 hex characters) - 1 EOL byte + 2 bytes for 1 16 bit ADC value 
-                            print(int(serialString[0:4], 16))     # The int('8A01', 16) function converts hex to 16 bit unsigned int - assumes leftmost bit is most significant 
-                        
-                        elif(len(serialString) == 34):
-                            print("ADC01 = %i ; AD02 = %i ; AD03 = %i ; ADC04 = %i ; AD05 = %i ; AD06 = %i ; AD07 = %i ; AD08 = %i" % (int(serialString[0:4], 16), int(serialString[4:8], 16), int(serialString[8:12], 16), int(serialString[12:16], 16), int(serialString[16:20], 16), int(serialString[20:24], 16), int(serialString[24:28], 16), int(serialString[28:32], 16)))     # The int('8A01', 16) function converts hex to 16 bit unsigned int - assumes leftmost bit is most significant
-                        # print(serialString.decode("uint8"))   #uint8array
-                        # break
+                        # if(serialString[Bytes_Transferred-1] != 10):    # 10 = 0x0a = '\n' LF character in Ascii 
+                        if(serialString[-2: ] != '0a'):    # 10 = 0x0a = '\n' LF character in Ascii 
+                            print("Transfer Error")
+                        else:
+                            Readings+=1
+                            print("Reading: = %i ; ADC01 = %i ; AD02 = %i ; AD03 = %i ; ADC04 = %i ; AD05 = %i ; AD06 = %i ; AD07 = %i ; AD08 = %i" % (Readings, int(serialString[0:4], 16), int(serialString[4:8], 16), int(serialString[8:12], 16), int(serialString[12:16], 16), int(serialString[16:20], 16), int(serialString[20:24], 16), int(serialString[24:28], 16), int(serialString[28:32], 16)))     # The int('8A01', 16) function converts hex to 16 bit unsigned int - assumes leftmost bit is most significant
+                            # print("ADC01 = %i ; AD02 = %i ; AD03 = %i ; ADC04 = %i ; AD05 = %i ; AD06 = %i ; AD07 = %i ; AD08 = %i" % (int(serialString[0:4], 16), int(serialString[4:8], 16), int(serialString[8:12], 16), int(serialString[12:16], 16), int(serialString[16:20], 16), int(serialString[20:24], 16), int(serialString[24:28], 16), int(serialString[28:32], 16)))     # The int('8A01', 16) function converts hex to 16 bit unsigned int - assumes leftmost bit is most significant
+
                     except:
                         pass
-            
 
         else:
             print("The Device connected to port %s is not an mbed Serial Port" % COM_Port)
