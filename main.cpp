735a7373dd610537037316845dbee4f4cbdf1f61
mpa31 <marnold212.ma212@gmail.com>
2021-11-05 11:55:06 +0000

    Serial Read Register Test

--- main.cpp
+++ main.cpp
@@ -1,73 +1,56 @@
-/* Copyright (c) 2018 Arm Limited
-*
-* SPDX-License-Identifier: Apache-2.0
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
 #include "mbed.h"
-#include <inttypes.h>
 
-#if !defined(MBED_SYS_STATS_ENABLED)
-#error [NOT_SUPPORTED] System statistics not supported
-#endif
+// Maximum number of element the application buffer can contain
+#define MAXIMUM_BUFFER_SIZE                                                  32
+// Create a BufferedSerial object with a default baud rate.
+static BufferedSerial serial_port(USBTX, USBRX);
 
-int main()
-{
-    mbed_stats_sys_t stats;
-    mbed_stats_sys_get(&stats);
+// Application buffer to receive the data
+char buf[MAXIMUM_BUFFER_SIZE] = "{0}";
 
-    printf("Mbed OS Version: %" PRId32 "\n", stats.os_version);
+int main(){
+    DigitalOut led(LED1);
+    // Set desired properties (9600-8-N-1).
+    serial_port.set_baud(9600);
+    serial_port.set_format(
+        /* bits */ 8,
+        /* parity */ BufferedSerial::None,
+        /* stop bit */ 1
+    );
 
-    /* CPUID Register information
-    [31:24]Implementer      0x41 = ARM
-    [23:20]Variant          Major revision 0x0  =  Revision 0
-    [19:16]Architecture     0xC  = Baseline Architecture
-                            0xF  = Constant (Mainline Architecture)
-    [15:4]PartNO            0xC20 =  Cortex-M0
-                            0xC60 = Cortex-M0+
-                            0xC23 = Cortex-M3
-                            0xC24 = Cortex-M4
-                            0xC27 = Cortex-M7
-                            0xD20 = Cortex-M23
-                            0xD21 = Cortex-M33
-    [3:0]Revision           Minor revision: 0x1 = Patch 1
-    */
-    printf("CPU ID: 0x%" PRIx32 "\n", stats.cpu_id);
+    uint32_t *Test_Addr = (uint32_t *)0x40048024U;  // 0x40047000U + 0x1024U
+    uint32_t num = 0;
+    uint32_t num_in = 0;
+    printf("Contents of Register with address %p = %lu\n", Test_Addr, *Test_Addr);
+    while(1)
+    {
 
-    /* Compiler IDs
-        ARM     = 1
-        GCC_ARM = 2
-        IAR     = 3
-    */
-    printf("Compiler ID: %d \n", stats.compiler_id);
+        led = 0;
+        num = 0;
+        num_in = 0;
+        // ThisThread::sleep_for(1s);
+        while (serial_port.readable()) {
+            led = 1;
+            ThisThread::sleep_for(1s); // Needed to get full input 
+            if ((num_in = serial_port.read(buf, sizeof(buf)))) {
+                num = num_in + 1;
 
-    /* Compiler versions:
-       ARM: PVVbbbb (P = Major; VV = Minor; bbbb = build number)
-       GCC: VVRRPP  (VV = Version; RR = Revision; PP = Patch)
-       IAR: VRRRPPP (V = Version; RRR = Revision; PPP = Patch)
-    */
-    printf("Compiler Version: %" PRId32 "\n", stats.compiler_version);
-
-    /* RAM / ROM memory start and size information */
-    for (int i = 0; i < MBED_MAX_MEM_REGIONS; i++) {
-        if (stats.ram_size[i] != 0) {
-            printf("RAM%d: Start 0x%" PRIx32 " Size: 0x%" PRIx32 "\n", i, stats.ram_start[i], stats.ram_size[i]);
+            }
         }
-    }
-    for (int i = 0; i < MBED_MAX_MEM_REGIONS; i++) {
-        if (stats.rom_size[i] != 0) {
-            printf("ROM%d: Start 0x%" PRIx32 " Size: 0x%" PRIx32 "\n", i, stats.rom_start[i], stats.rom_size[i]);
+        // uint32_t *Address;
+        // for(int x = 0; x < 4; x++){
+        //     uint32_t byte = buf[x];
+        //     Address |= (uint32_t *)(byte << (8*(3-x)));
+        // }
+
+        if(num > 1){
+            buf[num-1] = '\n';
+            buf[num-2] = num_in;
+            // (*buf) = *Address;
+            serial_port.write(buf, num);
         }
+        // printf("Address = %p Contents = %lu\n", Address, *Address);
     }
-    return 0;
+    
 }
+
